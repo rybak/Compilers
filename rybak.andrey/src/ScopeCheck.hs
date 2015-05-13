9cbc3df06c6e2149996450fca47cda3def324bd8
Andrey Rybak <rybak.a.v@gmail.com>
2015-05-13 15:42:22 +0300

    start writing ScopeCheck.hs

---
+++ rybak.andrey/src/ScopeCheck.hs
@@ -0,0 +1,15 @@
+module ScopeCheck where
+
+import Lang.Abs
+import Lang.ErrM
+
+import Control.Monad (unless, when)
+import Utils.SM
+import Data.Maybe
+import qualified Data.Map as M
+
+
+checkScope :: ParProgram -> BuildSt
+checkScope = undefined
+
+  
